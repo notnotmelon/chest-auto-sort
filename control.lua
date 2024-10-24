f8adb3e2f332a059fd30ef415bc1dffe9cc32829
notnotmelon <zacharypicco@gmail.com>
2024-10-24 15:56:21 -0500

    feat!: Updated for 2.0

--- control.lua
+++ control.lua
@@ -1,6 +1,13 @@
 local gui_type = defines.gui_type.entity
 local inventory = defines.inventory.chest
-local prototypes = {['container'] = true, ['logistic-container'] = true, ['infinity-container'] = true, ['linked-container'] = true}
+local prototypes = {
+	['container'] = true,
+	['logistic-container'] = true,
+	['infinity-container'] = true,
+	['linked-container'] = true,
+	['temporary-container'] = true,
+}
+
 script.on_event(defines.events.on_tick, function()
 	for _, player in pairs(game.connected_players) do
 		if player.opened_gui_type == gui_type and prototypes[player.opened.type] then
