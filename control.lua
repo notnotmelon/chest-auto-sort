8248fe3f8c71e9f39fbc600e5712e5b9a76d626b
notnotmelon <zacharypicco@gmail.com>
2024-10-24 16:01:56 -0500

    feat: Support sorting of cargo wagons

--- control.lua
+++ control.lua
@@ -1,17 +1,18 @@
 local gui_type = defines.gui_type.entity
-local inventory = defines.inventory.chest
 local prototypes = {
-	['container'] = true,
-	['logistic-container'] = true,
-	['infinity-container'] = true,
-	['linked-container'] = true,
-	['temporary-container'] = true,
+	['container'] = defines.inventory.chest,
+	['logistic-container'] = defines.inventory.chest,
+	['infinity-container'] = defines.inventory.chest,
+	['linked-container'] = defines.inventory.chest,
+	['temporary-container'] = defines.inventory.chest,
+	['cargo-wagon'] = defines.inventory.cargo_wagon,
 }
 
 script.on_event(defines.events.on_tick, function()
 	for _, player in pairs(game.connected_players) do
-		if player.opened_gui_type == gui_type and prototypes[player.opened.type] then
-			player.opened.get_inventory(inventory).sort_and_merge()
+		if player.opened_gui_type == gui_type then
+			local inventory = prototypes[player.opened.type]
+			if inventory then player.opened.get_inventory(inventory).sort_and_merge() end
 		end
 	end
 end)
